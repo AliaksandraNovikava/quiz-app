451d07f6b0ea6292f93ea5a9c840ed82ef63f51d
Aliaksandra Novikava <alexa_nov90@yahoo.de>
2023-05-24 21:12:20 +0200

    add media queries for tablet and some dark styling

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-const addAnswerButton = document.querySelector(
-  '[data-js="show-answer-button"]'
-);
-const addHiddenAnswer = document.querySelector('[data-js="hidden-answer"]');
-const addBookmark = document.querySelector('[data-js="bookmark-button"]');
+// const addAnswerButton = document.querySelector(
+//   '[data-js="show-answer-button"]'
+// );
+// const addHiddenAnswer = document.querySelector('[data-js="hidden-answer"]');
+// const addBookmark = document.querySelector('[data-js="bookmark-button"]');
 
-addAnswerButton.addEventListener("click", () => {
-  addHiddenAnswer.classList.toggle("answer");
-});
+// addAnswerButton.addEventListener("click", () => {
+//   addHiddenAnswer.classList.toggle("answer");
+// });
