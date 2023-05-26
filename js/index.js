8e7f0c7620a611ca926e4b024087cd10b0eaf82d
Aliaksandra Novikava <alexa_nov90@yahoo.de>
2023-05-26 11:30:08 +0200

    add toggle functionality to the card button

--- js/index.js
+++ js/index.js
@@ -1,19 +1,30 @@
-// const addAnswerButton = document.querySelector(
-//   '[data-js="show-answer-button"]'
-// );
-// const addHiddenAnswer = document.querySelector('[data-js="hidden-answer"]');
-// const addBookmark = document.querySelector('[data-js="bookmark-button"]');
+const addAnswerButton = document.querySelector(
+  '[data-js="show-answer-button"]'
+);
+const addHiddenAnswer = document.querySelector('[data-js="hidden-answer"]');
+const addBookmark = document.querySelector('[data-js="bookmark-button"]');
 
-// addAnswerButton.addEventListener("click", () => {
-//   addHiddenAnswer.classList.toggle("answer");
-// });
+addAnswerButton.addEventListener("click", function handleClick() {
+  addHiddenAnswer.classList.toggle("card__answer");
 
-const darkMode = document.querySelector('[data-js="checkbox"]');
-
-darkMode.addEventListener("click", () => {
-  if (document.body.classList.contains("body-dark")) {
-    document.body.classList.remove("body-dark");
+  const initialText = "Show answer";
+  if (
+    addAnswerButton.textContent
+      .toLowerCase()
+      .includes(initialText.toLowerCase())
+  ) {
+    addAnswerButton.textContent = "Hide answer";
   } else {
-    document.body.classList.add("body-dark");
+    addAnswerButton.textContent = initialText;
   }
 });
+
+// const darkMode = document.querySelector('[data-js="checkbox"]');
+
+// darkMode.addEventListener("click", () => {
+//   if (document.body.classList.contains("body-dark")) {
+//     document.body.classList.remove("body-dark");
+//   } else {
+//     document.body.classList.add("body-dark");
+//   }
+// });
