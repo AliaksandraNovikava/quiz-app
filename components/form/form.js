9ca90f8cca0013a583ba748ea0cc257ac6169ae3
Aliaksandra Novikava <alexa_nov90@yahoo.de>
2023-05-31 21:01:49 +0200

    add form field text counter

--- components/form/form.js
+++ components/form/form.js
@@ -1,6 +1,32 @@
 const form = document.querySelector('[data-js="form"]');
 const submitNewCard = document.querySelector('[data-js="submit-new-card"]');
 const newCardsList = document.querySelector('[data-js="new-cards-list"]');
+const inputCounterQuestion = document.querySelector(
+  '[data-js="input-counter-question"]'
+);
+const inputCounterAnswer = document.querySelector(
+  '[data-js="input-counter-answer"]'
+);
+const questionInput = document.querySelector('[data-js="question-input"]');
+const answerInput = document.querySelector('[data-js="answer-input"]');
+const maxLength = questionInput.getAttribute("maxlength");
+
+const updateInputCounterQuestion = (characters) => {
+  inputCounterQuestion.textContent = characters;
+};
+const updateInputCounterAnswer = (characters) => {
+  inputCounterAnswer.textContent = characters;
+};
+
+updateInputCounterQuestion(maxLength);
+updateInputCounterAnswer(maxLength);
+
+questionInput.addEventListener("input", () => {
+  updateInputCounterQuestion(maxLength - questionInput.value.length);
+});
+answerInput.addEventListener("input", () => {
+  updateInputCounterAnswer(maxLength - answerInput.value.length);
+});
 
 function createNewCard(question, answer, tag) {
   const newCard = document.createElement("li");
@@ -9,6 +35,11 @@ function createNewCard(question, answer, tag) {
   const newQuestion = document.createElement("h2");
   newQuestion.textContent = question;
   newQuestion.classList.add("card-question");
+
+  const cardButton = document.createElement("button");
+  cardButton.setAttribute("type", "button");
+  cardButton.classList.add("card__button");
+  cardButton.textContent = "Show answer";
 
   const newAnswer = document.createElement("p");
   newAnswer.textContent = answer;
@@ -21,20 +52,21 @@ function createNewCard(question, answer, tag) {
   newTag.classList.add("tags-li");
 
   newCardsList.append(newCard);
-  newCard.append(newQuestion, newAnswer, tagBox);
+  newCard.append(newQuestion, cardButton, newAnswer, tagBox);
   tagBox.append(newTag);
 }
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  //   const formData = new FormData(event.target);
-  //   const cardData = Object.fromEntries(formData);
   const cardData = event.target.elements;
   const question = cardData.formCardQuestion.value;
   const answer = cardData.formCardAnswer.value;
   const tag = "#" + cardData.formCardTag.value;
 
   event.target.reset();
+
+  updateInputCounterQuestion(maxLength);
+  updateInputCounterAnswer(maxLength);
 
   createNewCard(question, answer, tag);
 
